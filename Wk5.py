6a7f7975c9494ff149994635cca2b63b2f954e3c
Marcus Herstik <marcus@herstik.com>
2024-10-30 08:37:26 +1000

    Week 5

---
+++ Wk5.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 29 13:23:32 2024
+
+@author: marcus
+"""
+
+### Week 5
+
+i =0
+while i < 10:
+    i = i + 1
+    print(i)
+
+# else: 
+#     print("i is too high")
