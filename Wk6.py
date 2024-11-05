72f2d6e9ec99edea2d84c2d6b247dcdad113693b
Marcus Herstik <marcus@herstik.com>
2024-11-05 14:04:28 +1000

    Update Wk6.py

--- Wk6.py
+++ Wk6.py
@@ -7,19 +7,31 @@ Created on Tue Nov  5 13:07:23 2024
 """
 
 import datetime as dt
+from urllib import request
+
+import time
+# print(f"Time is {time.time()}")
+# print(f"GM Time is {time.gmtime()}")
+# print(f"C Time is {time.ctime()}")
 
 ## Create a Speedtester
 
 ## start a timer / get current time
-dl_start = dt.datetime.now(tz=None)
+# dl_start = dt.datetime.now(tz=None)  # changed to time.time
+dl_start = time.time()
 print(dl_start)
+
 ### Download a file
 
-pause = input("Press enter to stop the pause")
+dl_file = "https://github.com/Mherstik/Automation_Trainee_Nov2024/raw/refs/heads/main/20MB.zip"
+
+## download the file??
+with request.urlopen(dl_file) as response:
+    response.read()
 
 
 ## stop the timer
-dl_stop = dt.datetime.now(tz=None)
+dl_stop = time.time() # dt.datetime.now(tz=None)
 print(dl_stop)
 
 # Calculate time to download.
@@ -27,5 +39,10 @@ dl_delta = dl_stop - dl_start
 print(dl_delta)
 
 ## Convert the time to Mbps
+# speed = filesize / downloadtime
+speed = 
+print(f"Your internet is running at {speed}Mbps")
 
 ## 10MB file =  80 Mbits = factor of 8
+# 20MB = 160 Mbits
+
