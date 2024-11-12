eaa9f65c8901d13b168605708a0cb41ed6e42a9c
Marcus Herstik <marcus@herstik.com>
2024-11-12 15:04:56 +1000

    Week7Update

--- Wk7.py
+++ Wk7.py
@@ -9,6 +9,8 @@ Created on Tue Nov 12 13:46:20 2024
 import datetime as dt
 from urllib import request
 import time
+import socket
+import os.path
 
 
 # System time
@@ -18,16 +20,29 @@ def get_SystemTime():
 # Computer Name
 def get_ComputerName():
     ## get the hostname here!!
-    return 
+    name = socket.gethostname()
+    return name
 
 # IP-address
 def get_IPAddress():
+    
     pass
 
 # MAC-address
+def get_MACaddress():
+    pass
+
 # Processor Model
+def get_Processor():
+    pass
+
 # OperatingSystem
-    
+def get_OperatingSystem():
+    pass
+
+def print():
+    a = 1+1
+
 # Internet connection speed
 def speedtester():
     ## Create a Speedtester
@@ -63,10 +78,37 @@ def speedtester():
     
     
 currentTime = get_SystemTime()
-
 speed = speedtester()
 compName = get_ComputerName()
 
-print(currentTime, speed,compName)    
+## not implemented
+compIP = get_IPAddress()
+compMAC = get_MACaddress()
+
+
+### Store the speeds in a table or file
+
+### check if file exists.
+## if file DOES NOT exist... write the headers
+## if file exists... move to adding speed to file
+## else add speed to file
+print(currentTime, speed, compName)    
+
+filename = "Week7.csv"
+if os.path.exists(filename):
+    print("File exists.\nSkipping headers")
+    f = open(filename, "a")
+else:
+    f = open(filename, "a")
+    print("File does not exist.\nWriting headers")
+    f.write("Time/Date,Hostname,Speed\n")  ## make this optional
+
+f.write(f"{currentTime},")
+f.write(compName + ",")
+f.write(str(speed) + '\n')
+f.close()
+
+
+
     
     
